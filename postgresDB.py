cdcdb4ffe5f52cae53a0a041eec3d12a7d3fddd1
annar <anna.romanova0818@gmail.com>
2024-12-19 15:43:28 +0300

    FAT-3 add sqlalchemy

--- postgresDB.py
+++ postgresDB.py
@@ -1,39 +1,59 @@
-import psycopg2
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+# Базовый класс, от которого наследуются классы таблиц
+Base = declarative_base()
 
 
-try:
-    connection = psycopg2.connect(
-        host="localhost",
-        user="Anny",
-        password="123456",
-        database="FAT_db4",
-        port=5431
-    )
-    connection.autocommit = True
+# Создаем модели таблиц для бд
+class Author(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    author_name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
 
-    with connection.cursor() as cursor:
-        cursor.execute("""SELECT
-         authors.id AS author_id,
-         authors.author_name AS author_name,
-         posts.id AS post_id,
-         posts.title,
-         posts.body
-         FROM authors
-         JOIN posts
-         ON authors.id = posts.author_id""")
-        print(cursor.fetchall())
+    # Прописываем связь таблиц
+    posts = relationship('Post', order_by='Post.id', back_populates='author')
 
-    # with connection.cursor() as cursor:
-    #     cursor.execute("""INSERT INTO posts (title, body, author_id) VALUES
-    #     ('title_1', 'body_1', 1),
-    #     ('title_2', 'body_2', 2),
-    #     ('title_3', 'body_3', 1),
-    #     ('title_4', 'body_4', 3)""")
 
-except Exception as e:
-    print(f"Error: {e}")
+class Post(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    body = Column(String, nullable=False)
+    author_id = Column(Integer, ForeignKey('authors.id'))
 
-finally:
-    if 'connection' in locals() and connection:
-        connection.close()
+    # Прописываем обратную связь
+    author = relationship('Author', back_populates='posts')
 
+
+# Создаем движок
+engine = create_engine("postgresql+psycopg2://Anny:123456@localhost:5431/FAT_db4", echo=True) # будет выводить логи в терминал
+
+# Создаем сами таблицы в бд
+Base.metadata.create_all(engine) # metadata хранит информацию о всех таблицах, определенных через эти классы.
+
+# Создаем сессию для работы с бд
+Session = sessionmaker(bind=engine) # sessionmaker - фабрика, которая создает объекты сессий
+session = Session() # Создает экземпляр сессии, который можно использовать для работы с бд
+
+
+# Заполняю таблицу authors
+author1 = Author(author_name='Anna', age=29)
+author2 = Author(author_name='Pavel', age=33)
+author3 = Author(author_name='Inga', age=56)
+
+session.add_all([author1, author2, author3])
+session.commit()
+
+
+# Заполняю таблицу posts
+post1 = Post(title='title_1', body='body_1', author_id=1)
+post2 = Post(title='title_2', body='body_2', author_id=2)
+post3 = Post(title='title_3', body='body_3', author_id=1)
+post4 = Post(title='title_4', body='body_4', author_id=3)
+
+session.add_all([post1, post2, post3, post4])
+session.commit()
+
+session.close()
