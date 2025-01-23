1e3bdf3f42fa494f3103a97c57158be0b133544b
annar <anna.romanova0818@gmail.com>
2025-01-23 16:03:55 +0300

    FAT-5 update tests

--- tests/api/test_add_post.py
+++ tests/api/test_add_post.py
@@ -1,28 +1,45 @@
+from unittest.mock import ANY
+from uuid import uuid4
+
 import pytest
-from schemas.model import Post
+
+from db.connector import DatabaseConnector
+from db.models import Author, Post
 
 
 @pytest.mark.asyncio
-async def test_create_post_201(async_client, post_db: dict[int: Post]):
-    new_post_id = len(post_db) + 1
-    response_body = {"title": "new_test_title",
-                     "body": "new_test_body",
-                     "author_id": 1}
-    response = await async_client.post("/posts/", json=response_body)
-
-    assert response.status_code == 201
-    assert response.json() == {"id": new_post_id,
-                               "title": "new_test_title",
-                               "body": "new_test_body",
-                               "author_id": 1}
+@pytest.mark.usefixtures("prepare_author")
+async def test_create_post_201(async_client, post_db: DatabaseConnector, author: Author):
+    new_post_data = {"title": "test_title",
+                     "short_body": "test_short_body",
+                     "body": "test_body",
+                     "author_id": str(author.id)}
+    response = await async_client.post("/posts/", json=new_post_data)
+    assert response.status_code == 201, response.text
+    data = response.json()
+    assert data == {
+        "id": ANY,
+        "title": "test_title",
+        "body": "test_body",
+        "author": {
+            "id": str(author.id),
+            "name": author.name,
+            "age": author.age
+        }
+    }
+    async with post_db.session_maker() as session:
+        post_db = await session.get(Post, data["id"])
+    assert post_db.title == data["title"]
+    assert post_db.author_id == author.id
 
 
 @pytest.mark.asyncio
-async def test_create_post_404(async_client):
+async def test_create_post_400(async_client):
     response_body = {"title": "new_test_title",
                      "body": "new_test_body",
-                     "author_id": 10000}
+                     "short_body": "new_test_short_body",
+                     "author_id": str(uuid4())}
 
     response = await async_client.post("/posts/", json=response_body)
-    assert response.status_code == 404
-    assert response.json() == {"detail": "User not found"}
+    assert response.status_code == 400, response.text
+    assert response.json() == {"detail": f"Автор с id {response_body["author_id"]} не найден."}
--- tests/api/test_delete_post.py
+++ tests/api/test_delete_post.py
@@ -1,15 +1,24 @@
+from uuid import UUID, uuid4
+
 import pytest
+
+from db.connector import DatabaseConnector
+from db.models import Post
 
 
 @pytest.mark.asyncio
-async def test_delete_post_204(async_client):
-    response = await async_client.delete("/posts/1")
-    assert response.status_code == 204
+@pytest.mark.usefixtures("prepare_post")
+async def test_delete_post_204(async_client, post_db: DatabaseConnector, post_id: UUID):
+    response = await async_client.delete(f"/posts/{post_id}")
+    assert response.status_code == 204, response.text
     assert response.content == b""
+    async with post_db.session_maker() as session:
+        deleted_post = await session.get(Post, post_id)
+    assert deleted_post is None
 
 
 @pytest.mark.asyncio
 async def test_delete_post_404(async_client):
-    response = await async_client.delete("/posts/10000")
-    assert response.status_code == 404
-    assert response.json() == {"detail": "Post not found"}
+    response = await async_client.delete(f"/posts/{uuid4()}")
+    assert response.status_code == 404, response.text
+    assert response.json() == {"detail": "Пост не найден"}
