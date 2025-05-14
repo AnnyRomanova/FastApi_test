ea965e4fb2ab3900011b7faa5738e135d11b0e16
AnnyRomanova <anna.romanova0818@gmail.com>
2025-05-14 15:46:35 +0300

    update docker-compose, settings

--- src/core/settings.py
+++ src/core/settings.py
@@ -31,6 +31,8 @@ class Settings(BaseSettings):
     class Config:
         case_sensitive = True
         env_nested_delimiter = "__"
+        env_file = ".env"
+        extra = "ignore"
 
 
 def get_settings() -> Settings:
